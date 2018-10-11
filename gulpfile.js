367dfc5ccdc5743a3c8b5776963f731e9da766ee
FronteraDevs <test@fronteraswarm.onmicrosoft.com>
2018-10-11 10:34:43 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", function() {
+  gulp.watch("./sass/*.scss", ["sass"]);
+});
+
+var uglifycss = require("gulp-uglifycss");
+
+gulp.task("css", function() {
+  gulp
+    .src("./css/*.css")
+    .pipe(
+      uglifycss({
+        maxLineLen: 80,
+        uglyComments: true
+      })
+    )
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("run", [("sass", "css")]);
+
+gulp.task("watch", function() {
+  gulp.watch("./sass/*.scss", ["sass"]);
+  gulp.watch("./css/*.css", ["css"]);
+});
+
+gulp.task("default", ["run", "watch"]);
