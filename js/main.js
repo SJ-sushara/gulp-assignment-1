eaa3a1c06b5f4447122c1fb0bfab0badb5d2a52a
FronteraDevs <test@fronteraswarm.onmicrosoft.com>
2018-10-16 16:15:00 +0530

    files updated

--- js/main.js
+++ js/main.js
@@ -18,3 +18,17 @@ function navbarToggle() {
 }
 navbarToggle();
 window.onresize = navbarToggle;
+
+//script for parallax effect on banner
+
+// var parallax = document.querySelector(".parallax");
+// window.addEventListener("scroll", function() {
+//   var scrolledHeight = window.pageYOffset,
+//     limit = parallax.offsetTop + parallax.offsetHeight;
+//   if (scrolledHeight > parallax.offsetTop && scrolledHeight <= limit) {
+//     parallax.style.backgroundPositionY =
+//       (scrolledHeight - parallax.offsetTop) / 1.5 + "px";
+//   } else {
+//     parallax.style.backgroundPositionY = "0";
+//   }
+// });
