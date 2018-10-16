1cadb143a4d9e07a10545df7fadaa6aa625d2582
FronteraDevs <test@fronteraswarm.onmicrosoft.com>
2018-10-16 14:30:05 +0530

    window resize is added in script

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@
 // var y = document.getElementById("navMenu");
 // var y = this;
 
-if (document.body.offsetWidth <= 767) {
-  document.getElementById("navMenu").classList.add("hidden");
-  document.getElementById("toggleIcon").onclick = function(e) {
-    var menuClasses = document.getElementById("navMenu").classList;
-    if (menuClasses.contains("hidden")) {
-      menuClasses.remove("hidden");
-    } else {
-      menuClasses.add("hidden");
-    }
-  };
+function navbarToggle() {
+  if (document.body.offsetWidth <= 767) {
+    document.getElementById("navMenu").classList.add("hidden");
+    document.getElementById("toggleIcon").onclick = function(e) {
+      var menuClasses = document.getElementById("navMenu").classList;
+      if (menuClasses.contains("hidden")) {
+        menuClasses.remove("hidden");
+      } else {
+        menuClasses.add("hidden");
+      }
+    };
+  }
 }
+navbarToggle();
+window.onresize = navbarToggle;
